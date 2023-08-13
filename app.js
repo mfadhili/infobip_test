afa7cf037d8373311714b980d53faaacc281a832
mfadhili <mfadhili@github.com>
2023-08-13 16:36:04 +0300

    test ok

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+var {Infobip, AuthType} = require("@infobip-api/sdk/")
 
 
 const app = express();
@@ -20,6 +21,27 @@ app.post('/invoke',function(req,res) {
 
     res.send(req.body);
 })
+
+let infobip = new Infobip({
+    baseUrl: "zjpnwk.api.infobip.com",
+    apiKey: "2a2fbf8a5d74d4c46a32de9bcab9b53d-0ab86c65-607e-48d0-9be2-93c0d4486eaa",
+    authType: AuthType.ApiKey,
+});
+
+app.post('/send', async function (req, res) {
+    let response = await infobip.channels.whatsapp.send({
+        type: "text",
+        from: "447860099299",
+        to: "254742764377",
+        content: {
+            text: "Hello World",
+        },
+    });
+
+    console.log(response);
+
+})
+
 
 app.listen(port,() =>{
     console.log('Server started! At http://localhost:' + port)
